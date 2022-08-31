ac799be25a4b10fa64b40ffb21da62a6531010e2
alimurad <rxalimurad@gmail.com>
2022-08-31 13:45:19 +0500

    Initial Commit

---
+++ Coffe menu/Coffe_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffe_menuApp.swift
+//  Coffe menu
+//
+//  Created by Ali Murad on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Coffe_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
