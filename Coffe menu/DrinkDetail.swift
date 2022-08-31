2933e6359fd581b028e95c949f7b7976a9793b2f
Murad <alimurad@volutiontech.com>
2022-09-01 02:00:25 +0500

    --- Add Cart button in detail view

--- Coffe menu/DrinkDetail.swift
+++ Coffe menu/DrinkDetail.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct DrinkDetail: View {
+    @State private var showingAlert = false
+    
     var drink: Drink
     
     var body: some View {
@@ -32,7 +34,26 @@ struct DrinkDetail: View {
                     Spacer()
                 }
             }
+            .listRowInsets(EdgeInsets())
+            Text(drink.description)
+                .foregroundColor(.primary)
+                .font(.body)
+                .lineLimit(5)
+                .padding()
             
+            HStack {
+                Spacer()
+                OrderButton(showAlert:$showingAlert, drink: drink).b
+                Spacer()
+                
+            }
+            .padding(.top, 50)
+            
+        }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarHidden(true)
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Added to Basket!"),dismissButton: .default(Text("Ok")))
         }
     }
 }
@@ -40,5 +61,25 @@ struct DrinkDetail: View {
 struct SDrinkDetail_Previews: PreviewProvider {
     static var previews: some View {
         DrinkDetail(drink: drinkData[0])
+    }
+}
+
+
+struct OrderButton: View {
+    @Binding var showAlert: Bool
+    var drink: Drink
+    var body: some View {
+        Button {
+            print ("ordered \(drink.name)")
+            self.showAlert.toggle()
+        } label: {
+            Text("Add to basket")
+        }
+        .frame(width: 200, height: 50)
+        .foregroundColor(.white)
+        .background(Color.blue)
+        .font(.headline)
+        .cornerRadius(10)
+
     }
 }
